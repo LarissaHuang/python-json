479b3eb4fa3ee42f9e00630f80995bcccd629bf3
Larissa Huang <35763731+LarissaHuang@users.noreply.github.com>
2021-04-02 15:43:43 -0700

    possibleSlots test

--- python-json.py
+++ python-json.py
@@ -1,43 +1,19 @@
 import json
-import datetime
 from datetime import datetime
 from datetime import timedelta
 
 with open("meetings.json") as f:
     meetingData = json.load(f)
 
-
-# write
-#     with open("replayScript.json", "w") as jsonFile:
-#     json.dump(data, jsonFile)
-
 with open("possibleSlots.json") as f:
     possibleSlots = json.load(f)
 
-# totalEmployees = len(employeeData["employees"])
-
 for meeting in meetingData["meetings"]:
-    # print(meeting["participants"], meeting["description"])
-    d = meeting["description"]
+    busyBlockStart = meeting["startTime"]
+    busyBlockEnd = meeting["endTime"]
 
-    bookedMeetingStart = meeting["startTime"]
-    bookedMeetingEnd = meeting["endTime"]
-
-    # print("number of participants " + str(allParticipants))
-
-# print("total emps" + str(totalEmployees))
-
-# for employee in employeeData["employees"]:
-#     # print(employee["id"])
-#     y = employee["id"]
-#     n = employee["name"]
-#     m = employee["meetings"]
-
-max_part_list = 3
-
+# looping through possible meeting times
 for possibleSlot in possibleSlots["possibleSlots"]:
-    # print(employee["id"])
-    curr_part_list = 0
 
     possibleStart = datetime.strptime(possibleSlot["startTime"], "%b %d %Y %I:%M%p")
     possibleEnd = datetime.strptime(possibleSlot["endTime"], "%b %d %Y %I:%M%p")
@@ -47,47 +23,74 @@ for possibleSlot in possibleSlots["possibleSlots"]:
     with open("employees.json") as f:
         employeeData = json.load(f)
 
-    position = 0
-    for employee in employeeData["employees"]:
-        print("checking for employee", employee["id"])
-        # print("curr_parts", curr_part_list)
+    maxParticipants = len(employeeData["employees"])
 
-        if curr_part_list == max_part_list:
-            print("max participants reached")
+    # initialize potentialParticipants as 0
+    potentialParticipants = 0
+
+    # initialize position as 0, used for writing into json later
+    position = 0
+
+    # looping through each employee in the employees table
+    for employee in employeeData["employees"]:
+        # print("checking for employee", employee["id"])
+
+        if potentialParticipants == maxParticipants:
+            print("Good news! All employees are available for this meeting")
             break
         else:
-            meeting_list = employee["meetings"]
+            employeeMeetingList = employee["meetings"]
+
+            # initialize overlap as false
             overlap = False
 
-            # print(meeting_list)
+            # looping through each meeting in an employee's schedule
+            for meetingId in employeeMeetingList:
 
-            for meeting_id in meeting_list:
-                # print("meeting id", meeting_id)
+                # looping through meetings in the meetings table
                 for meeting in meetingData["meetings"]:
-                    if meeting_id == meeting["id"]:
 
-                        bookedMeetingStart = datetime.strptime(
+                    # checking to see if an employee is part of a meeting
+                    if meetingId == meeting["id"]:
+
+                        busyBlockStart = datetime.strptime(
                             meeting["startTime"], "%b %d %Y %I:%M%p"
                         )
-                        bookedMeetingEnd = datetime.strptime(
+                        busyBlockEnd = datetime.strptime(
                             meeting["endTime"], "%b %d %Y %I:%M%p"
                         )
-                        # break
 
-                t = bookedMeetingStart
+                t = busyBlockStart
 
-                while t <= bookedMeetingEnd:
+                # assuming busyBlockStart is before busyBlockEnd
+                while t <= busyBlockEnd:
+
+                    # if the potential meeting start time is during an already-booked meeting
                     if t >= possibleStart and t <= possibleEnd:
-                        # print("not possible meeting")
+
                         overlap = True
                         break
+
+                    # increment time by 15 minutes
                     t = t + timedelta(minutes=15)
 
-                # print("reached overlap")
             if overlap == False:
-                curr_part_list += 1
+
+                # if no overlap found, add employee to potential participants
+                potentialParticipants += 1
+
                 print("new participant added")
-                meeting_list.append(possibleSlot["id"])
+
+                # print(
+                #     "total potential participants",
+                #     potentialParticipants,
+                #     "for potential meeting id",
+                #     possibleSlot["id"],
+                # )
+
+                # this conditional prevents duplicate insertions of meetings into employee schedule
+                if possibleSlot["id"] not in employeeMeetingList:
+                    employeeMeetingList.append(possibleSlot["id"])
 
                 # for i in employeeData:
                 #     print("before adding, emp list", i)
@@ -104,22 +107,17 @@ for possibleSlot in possibleSlots["possibleSlots"]:
                     jsonFile.seek(0)
                     json.dump(employeeData, jsonFile)
 
+            print(
+                "total potential participants",
+                potentialParticipants,
+                "for potential meeting id",
+                possibleSlot["id"],
+            )
             #    update meeting list
-
         position += 1
         # find possible employees
-    if curr_part_list == 0:
+    if potentialParticipants == 0:
         print("no possible time slot")
-    # booked meeting 10-11
-    # possible meeting 3-4
-    # people who are busy
-    # y in p
-    # print(n + " is busy")
-
-    # # people who are free
-    # y not in p
-    # print(n + " is free")
-    # print("add to " + n + "'s calender")
 
 #    1. iterate through possible time slots
 #     9-10
